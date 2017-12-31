c71a4acf0a3ad2d780f84fac20f75ed3a53132be
Mohamed Hayibor <mohamedhayibor@gmail.com>
2017-12-31 09:41:00 -0500

    Add ticker prices subscription - delete extraneous comments

--- src/cljs/atfides/subs.cljs
+++ src/cljs/atfides/subs.cljs
@@ -2,36 +2,19 @@
   (:require [re-frame.core :as rf]
             [atfides.db :as db]))
 
-;; -- High level architechture:
-;; 1. App db
-;; 2. Extraction
-;; 3. Computation (derived from 2)
-;; 4. View (hiccup -> reagent)
-
-
-;; -- Layer 2 -------------------------------------
-;; -- accessor
-;; usage (subscribe [:local-pub-keys])
+;; -- Only layer 2 accessors
+;; -- Usage (subscribe [:local-pub-keys])
 (rf/reg-sub
  :local-pub-keys
  (fn [db event-vec]
    (println ">>> Inside reg :local-pub-keys ------>")
    (println ">>> db: " db)
-   (println ">>> vec1: " event-vec)
    (:local-pub-keys db)))
 
-
-
-
-;; -- Layer 3 --------------------------------------
-;; a subscription handler is a function which re-runs
-;; every time the <> state changes
-;;
-;; reg-sub allows you to:
-;; 1. a function which returns the input signals.
-;; > return either a single signal or a vector (map) of signals
-;;
-;; 2. a function that does the computation.
-;; > input values -> derived values
-
+(rf/reg-sub
+  :ticker-prices
+  (fn [db _]
+    (println ">>> Inside reg :ticker-prices ------>")
+    (println ">>> db: " db)
+    (:ticker-prices db)))
 
